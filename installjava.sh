94818162515fd334d0841ebdf063416aad5c8c6c
Vishwanath Jayaraman <vishwanathj@hotmail.com>
2017-08-22 16:41:24 -0500

    Added openjdk install ansible scripts

--- installjava.sh
+++ installjava.sh
@@ -9,13 +9,14 @@ read HOST
 echo "enter ssh username:"
 read USER
 
-echo "enter java version (only 6, 7, 8 and 9 are supported at this time):"
-read JAVA_VERSION
+#echo "enter java version (only 6, 7, 8 and 9 are supported at this time):"
+#read JAVA_VERSION
 
-echo "enter java type (only openjdk or oracle is supported)"
-read JAVA_TYPE
+#echo "enter java type (only openjdk or oracle is supported)"
+#read JAVA_TYPE
 
-ansible-playbook -i $HOST, installjava.yaml -u $USER -kK -e user=$USER --extra-vars="java_version=$JAVA_VERSION java_type=$JAVA_TYPE"
+#ansible-playbook -i $HOST, installjava.yaml -u $USER -kK -e user=$USER --extra-vars="java_version=$JAVA_VERSION java_type=$JAVA_TYPE"
 
+ansible-playbook -i $HOST, installjava.yaml -u $USER -kK -e user=$USER
 
 
